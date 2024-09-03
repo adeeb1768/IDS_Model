b97715a539a886a74690cf64bf458c463483c4d1
adeeb_kobash <84805534+adeeb1768@users.noreply.github.com>
2024-09-04 01:15:15 +0300

    Update process_test_data_file.py

--- processing_code/process_test_data_file.py
+++ processing_code/process_test_data_file.py
@@ -9,14 +9,14 @@ InteractiveShell.ast_node_interactivity = "all"
 df = pd.read_csv(r'C:\Users\adeeb\Desktop\master thesis\test file\part-00168-363d1ba3-8ab5-4f96-bc25-4d5862db7cb9-c000.csv')
 
 
-# Filter data by label (optional)
+# Filter data by label 
 filtered_labels = ['DDoS-RSTFINFlood', 'DDoS-TCP_Flood', 'DDoS-ICMP_Flood', 'DoS-UDP_Flood', 'DoS-SYN_Flood',
                    'Mirai-greeth_flood', 'DDoS-SynonymousIP_Flood', 'Mirai-udpplain', 'DDoS-SYN_Flood', 'DDoS-PSHACK_Flood',
                    'DDoS-UDP_Flood', 'BenignTraffic']
 df = df[df['label'].isin(filtered_labels)]  # Filter based on specified labels (optional)
 
 
-# Sample data (Optional)
+# Sample data 
 df = df.groupby('label', group_keys=False).apply(lambda x:x.sample(4000))
 
 filtered_data = df[['IAT', 'Rate', 'Header_Length', 'Number', 'flow_duration', 'syn_count', 'Variance','Protocol Type',
